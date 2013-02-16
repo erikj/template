1a4566a785d8963be6708ea1e7332f63ded863a3
erikj <erik@erikj.me>
2013-02-16 16:15:13 -0700

    removing unused less-rails and therubyracer asset gems

--- some-rails-template.rb
+++ some-rails-template.rb
@@ -5,8 +5,5 @@ gem 'unicorn'
 gem 'haml-rails'
 
 gem_group :assets do
-  # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  gem 'therubyracer', :platforms => :ruby
-  gem 'less-rails'
   gem 'twitter-bootstrap-rails'
 end
