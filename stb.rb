35805e5e431c512d545b2b668bfdae888c6bbee9
erikj <erik@erikj.me>
2013-02-17 21:15:55 -0700

    updating generated app templates

--- stb.rb
+++ stb.rb
@@ -16,7 +16,26 @@ gem_group :assets do
   gem 'twitter-bootstrap-rails'
 end
 
-run 'bundle install'
+if yes? "Do you want to run `bundle install` and initialize Twitter Bootstrap?"
+  run 'bundle install'
 
-generate "bootstrap:install", "static"
-generate "bootstrap:layout", "application", "fluid"
+  generate "bootstrap:install", "static"
+  generate "bootstrap:layout", "application", "fluid"
+else
+
+
+  # populate application layout w/ Slim
+  file "app/views/layouts/application.html.slim", <<-EOF.gsub(/^ {4}/, '')
+    doctype
+    html
+      head
+        title #{app_name.humanize}
+        = stylesheet_link_tag    "application", :media => "all"
+        = javascript_include_tag "application"
+        = csrf_meta_tags
+      body
+        = yield
+    EOF
+
+
+end
